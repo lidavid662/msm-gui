a1e278083e006c268df7053e812decf152ea9ef6
lidavid662 <146143936+lidavid662@users.noreply.github.com>
2023-11-06 00:40:48 +0000

    help

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,53 @@
 class MoviesController < ApplicationController
+  def update
+    # Get the ID out of params
+    m_id = params.fetch("the_id")
+
+    # Look up the existing record
+    matching_records = Movie.where({ :id => m_id })
+    the_movie = matching_records.at(0)
+
+    # Overwrite each column
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
+
+  def create
+
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
